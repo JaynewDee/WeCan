d1d1adf1901f8049505d06dadb880d93f6e38d55
NuminousBlue <jdiehl2236@gmail.com>
2021-12-20 17:46:25 -0600

    initialize mongoose db models

--- server.js
+++ server.js
@@ -5,16 +5,18 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+     extended: true
+}));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+     useNewUrlParser: true,
+     useFindAndModify: false
 });
 
 app.listen(PORT, () => {
      console.log(`App running on port ${PORT}!`);
-   });
+});
