c571e0bed4c8b66563f99b9113637753028abff8
NuminousBlue <jdiehl2236@gmail.com>
2021-12-22 18:22:09 -0600

    all routes functioning without known errors

--- routes/workouts.js
+++ routes/workouts.js
@@ -5,63 +5,76 @@ const Workout = require('../models/Workout')
 
 
 workouts.get("/", (req, res) => {
-     Workout.aggregate([{
-               $addFields: {
-                    totalDuration: {
-                         $sum: "$exercises.duration",
+     try {    
+          Workout.aggregate([{
+                    $addFields: {
+                         totalDuration: {
+                              $sum: "$exercises.duration",
+                         },
                     },
-               },
-          }, ])
-          .sort({
-               date: -1
-          }) //date parameter in workout.js
-          .then((dbWorkout) => {
-               res.json(dbWorkout);
-          })
-          .catch((err) => {
-               res.status(400).json(err);
-          });
+               }, ])
+               .sort({
+                    date: -1
+               }) //date parameter in workout.js
+               .then((dbWorkout) => {
+                    res.json(dbWorkout);
+               })
+               .catch((err) => {
+                    res.status(400).json(err);
+               });
+          }
+     catch (err){console.log(err)}
 });
 
 workouts.get("/range", (req, res) => {
-     Workout.aggregate([{
-               $addFields: {
-                    totalDuration: {
-                         $sum: "$exercises.duration"
-                    }, //seeds
-                    totalWeight: {
-                         $sum: "$exercises.weight"
-                    }, //seeds
-               },
-          }, ])
-          .limit(7) // 7 days in the week
-          .sort({
-               date: -1
-          }) //date parameter in workout.js
-          .then((dbWorkout) => {
-               res.json(dbWorkout);
-          })
-          .catch((err) => {
-               res.status(400).json(err);
-          });
+     try {
+          Workout.aggregate([{
+                    $addFields: {
+                         totalDuration: {
+                              $sum: "$exercises.duration"
+                         },
+                         totalWeight: {
+                              $sum: "$exercises.weight"
+                         }, 
+                              },
+                         }])
+               .limit(7)
+               .sort({
+                    date: -1
+               }) 
+               .then((workout) => {
+                    res.json(workout);
+               })
+          }
+     catch (err){console.log(err)}
 });
 
 workouts.put('/:id', ({ body, params }, res) => {
-     Workout.findByIdAndUpdate({
-               _id: params.id
-          }, {
-               $push: {
-                    exercises: body
-               }
-          }, {
-               new: true
-          })
-          .then((dbWorkout) => {
-               res.json(dbWorkout);
-          })
-          .catch((err) => {
-               res.status(400).json(err);
-          });
+     try {
+          Workout.findByIdAndUpdate({
+                    _id: params.id
+               }, {
+                    $push: {
+                         exercises: body
+                    }
+               }, {
+                    new: true
+               })
+               .then((workout) => {
+                    res.json(workout);
+               })
+          }
+     catch(err){console.log(err)}
+})
+
+workouts.post('/', ({ body }, res) => {
+     try {
+          Workout.create(body).
+               then((workout) => {
+                    res.json(workout)
+               })
+          }
+     catch (err) {console.log(err)}
 })
 
 
